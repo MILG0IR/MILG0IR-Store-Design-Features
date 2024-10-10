dddafa7ec4bd412a508b93b382b65b16a302890d
Robert Smith <RSmith_20@outlook.com>
2024-10-10 13:13:20 +0100

    Update admin.js
    Update to v0.0.3

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,16 +1,16 @@
 // Example of a basic script to add interactivity
+
+
 jQuery(document).ready(function() {
     console.log('MILG0IR Store Design & Features loaded');
-    
+	// Hide all sections
+	sections.forEach(section => section.classList.remove('active'));
+
     const sections = document.querySelectorAll('.milg0ir > .responsive-wrapper');
     const showSection = () => {
         const hash = window.location.hash.substring(1); // Get the ID from the URL (e.g., "1", "2")
-		console.log(hash)
-        
-        // Hide all sections
-        sections.forEach(section => section.classList.remove('active'));
 
-        // Show the section that matches the hash in the URL
+		// Show the section that matches the hash in the URL
         const targetSection = document.getElementById(hash);
         if (targetSection) {
             targetSection.classList.add('active');
